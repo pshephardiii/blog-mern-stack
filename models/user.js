9ab2d09ae494cc6c7dbd4cc69112c3ccb0b36c68
Paul Shephard <paulshephard598@gmail.com>
2024-02-20 19:07:22 -0500

    initial commit

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const { model, Schema } = require('mongoose')
+const bcrypt = require('bcrypt') 
+const jwt = require('jsonwebtoken') 
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Blog' }]
+} , {
+  timestamps: true
+})
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8)
+  }
+  next()
+})
+
+userSchema.methods.generateAuthToken = async function() {
+  const token = jwt.sign({ _id: this._id }, process.env.SECRET)
+  return token
+}
+
+const User = model('User', userSchema)
+
+module.exports = User
